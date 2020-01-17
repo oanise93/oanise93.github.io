dab69815e2c4b821a66b62530846a7cb834cc000
Olabode <oanise93@gmail.com>
2020-01-16 19:25:21 -0500

    adding keeper of the machines post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -8,4 +8,9 @@ var store = [{
         "excerpt":"Usability Smells: An Analysis of Developers’ Struggle With Crypto Libraries Patnaik et al., SOUPS 2019 While at SOUPS, this particular paper stood out because it related to some work that my colleagues and I were doing surrounding certificate chain validation. Cryptography is hard and using cryptographic APIs can be difficult...","categories": ["paper summary"],
         "tags": ["security"],
         "url": "http://localhost:4000/paper%20summary/usability-smells/",
+        "teaser":null},{
+        "title": "Keepers of the Machines: Examining How System Administrators Manage Software Updates For Multiple Machines",
+        "excerpt":"Keepers of the Machines: Examining How System Administrators Manage Software Updates Li et al., SOUPS 2019 Continuing through the SOUPS proceedings, I wanted to take some time to write about this paper by Frank Li, et. al. Their work focusing on the perspective of the administrator as it relates to...","categories": ["paper summary"],
+        "tags": ["security"],
+        "url": "http://localhost:4000/paper%20summary/keepers/",
         "teaser":null}]
